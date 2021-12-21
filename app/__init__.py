877bf94410816d922bcd66b6ce8f9e4a6f0ffd01
Notis Siokas <40504369@live.napier.ac.uk>
2021-12-21 12:29:42 +0000

    Database migration possible

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    #app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = 'p9Bv<7Eid9%$j01'
-SQLALCHEMY_DATABASE_URI = 'mysql+connector://super_admin1:DandelionSA2022@localhost/dandelion_db1'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://super_admin1:DandelionSA2022@localhost/dandelion_db1'
