ee5d39c63433a68d6b124b235053aff2f86ed08c
multivac <davidmccluskey1@gmail.com>
2022-03-17 11:22:25 +0000

    Fixed deepmerge error

--- React/dandelion/src/templates/using-dsg.js
+++ React/dandelion/src/templates/using-dsg.js
@@ -2,10 +2,8 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 const UsingDSG = () => (
   <Layout>
-    <Seo title="Using DSG" />
     <h1>Hello from a DSG Page</h1>
     <p>This page is not created until requested by a user.</p>
     <p>
