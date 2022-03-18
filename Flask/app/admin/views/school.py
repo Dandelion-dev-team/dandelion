c18d954eb88b73888c7143164ca9063cb07fb118
Notis Siokas <76698442+NotisSiokas@users.noreply.github.com>
2022-03-18 20:55:48 +0000

    New imageupload route for school / subdirectory creation

--- Flask/app/admin/views/school.py
+++ Flask/app/admin/views/school.py
@@ -1,12 +1,18 @@
-from flask import abort, request, jsonify
+import os
+
+from flask import abort, request, jsonify, current_app
 from flask_json import json_response
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy import inspect
+from werkzeug.utils import secure_filename
+
 from app.admin import admin
-from app.models import School
+from app.models import School, Img
 from app import db
 from app.utils.auditing import audit_create, audit_update, prepare_audit_details, audit_delete
 from app.utils.functions import row2dict, jwt_user
+from app.utils.images import allowed_file
+from PIL import Image
 
 
 @admin.route('/school', methods=['GET'])
@@ -22,18 +28,18 @@ def add_school():
     current_user = jwt_user(get_jwt_identity())
     data = request.get_json()
     school = School(
-        authority_id = data['authority_id'],
-        name = data['name'],
-        address_line_1 = data['address_line_1'],
-        address_line_2 = data['address_line_2'],
-        town = data['town'],
-        postcode = data['postcode'],
-        telephone = data['telephone'],
-        email = data['email'],
-        school_image_link = data['school_image_link'],
-        status = data['status'],
-        latitude = data['latitude'],
-        longitude = data['longitude']
+        authority_id=data['authority_id'],
+        name=data['name'],
+        address_line_1=data['address_line_1'],
+        address_line_2=data['address_line_2'],
+        town=data['town'],
+        postcode=data['postcode'],
+        telephone=data['telephone'],
+        email=data['email'],
+        school_image_link=data['school_image_link'],
+        status=data['status'],
+        latitude=data['latitude'],
+        longitude=data['longitude']
     )
 
     db.session.add(school)
@@ -63,7 +69,6 @@ def getOneSchool(id):
     school_data['status'] = school.status
 
     return jsonify({'school': school_data})
-
 
 
 @admin.route('/school/<int:id>', methods=['PUT'])
@@ -107,7 +112,7 @@ def delete_school(id):
     current_user = jwt_user(get_jwt_identity())
     school_to_delete = School.query.filter_by(id=id).first()
     if not school_to_delete:
-        return jsonify({"message" : "No school found"})
+        return jsonify({"message": "No school found"})
 
     audit_details = prepare_audit_details(inspect(School), school_to_delete, delete=True)
     db.session.delete(school_to_delete)
@@ -121,4 +126,56 @@ def delete_school(id):
 
     except Exception as e:
         db.session.rollback()
-        abort(409,e.orig.msg)
+        abort(409, e.orig.msg)
+
+
+@admin.route('/school/<int:id>/uploadImage', methods=['POST'])
+def upload_school_image(id):
+    dummy = request
+
+    id = id
+    id = str(id)
+    pic = request.files['pic']
+
+    if not pic:
+        return jsonify({"No pic uploaded"}), 400
+
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+
+    img = Img(mimetype=mimetype, name=filename)
+
+    db.session.add(img)
+    db.session.commit()
+
+    # check if the post request has the file part
+    if 'pic' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+
+    pic = request.files['pic']
+
+    if pic.filename == '':
+        resp = jsonify({'message': 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+
+    if pic and allowed_file(pic.filename):
+        filename = secure_filename(pic.filename)
+        newdir = (os.path.join(current_app.config['IMAGE_UPLOADS_SCHOOL'], id))
+        if not os.path.exists(newdir):
+            os.makedirs(newdir)
+        pic.save(os.path.join(newdir, id + filename))
+        pic = Image.open(pic)
+        pic.save(os.path.join(newdir, id + '.png'))
+        resp = jsonify({'message': 'Image successfully uploaded'})
+        resp.status_code = 201
+        return resp
+
+    else:
+        resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp.status_code = 400
+        return resp
+
+    return jsonify({"School images has been uploaded"}), 200
