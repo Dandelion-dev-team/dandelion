234b2870c787a0e15a557e781374dcaf0fbee16d
Notis Siokas <76698442+NotisSiokas@users.noreply.github.com>
2022-03-21 14:02:56 +0000

    Full size and Thumbnail size generation and cropping (only school)

--- Flask/app/admin/views/school.py
+++ Flask/app/admin/views/school.py
@@ -165,17 +165,28 @@ def upload_school_image(id):
 
         filename = secure_filename(pic.filename)
 
+        # Saves the original file with original name and filetype in while creating a new unique id directory
         newdir = (os.path.join(current_app.config['IMAGE_UPLOADS_SCHOOL'], id))
         if not os.path.exists(newdir):
             os.makedirs(newdir)
-        pic.save(os.path.join(newdir, id + filename))
+        pic.save(os.path.join(newdir, filename))
 
+        # Transforms uploaded image into .png file, name changing into [id].png
         pic = Image.open(pic)
         pic.save(os.path.join(newdir, id + '.png'))
 
-        full_size = pic.resize((590, 330 ))
+        # Saves the image in predetermined "full size" dimensions (590, 330). Saves it as [id]_full.png in proper directory
+        full_size = pic.resize((590, 330))
         full_size.save(os.path.join(newdir, id + '_full.png'))
 
+        # Image cropping and resising into the "full size" dimensions. Alternative to full size. Saving it as [id]_cropped_full.png
+        cw, ch = 590, 330
+        w, h = pic.size
+        box = w // 2 - cw // 2, h // 2 - ch // 2, w // 2 + cw // 2, h // 2 + ch // 2
+        cropped_image = pic.crop(box)
+        cropped_image.save(os.path.join(newdir, id + '_cropped_full.png'))
+
+        # Creates thumbnail, saving as [id]_thumb.png
         thumb_size = pic.resize((115, 90))
         thumb_size.save(os.path.join(newdir, id + '_thumb.png'))
 
