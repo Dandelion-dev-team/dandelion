dc13c2fe45c5bd32c2b13744d9be55781681e9ed
Notis Siokas <76698442+NotisSiokas@users.noreply.github.com>
2022-03-21 14:23:44 +0000

    Image processing is held in app/utils/images.py

--- Flask/app/admin/views/school.py
+++ Flask/app/admin/views/school.py
@@ -11,7 +11,7 @@ from app.models import School, Img
 from app import db
 from app.utils.auditing import audit_create, audit_update, prepare_audit_details, audit_delete
 from app.utils.functions import row2dict, jwt_user
-from app.utils.images import allowed_file
+from app.utils.images import allowed_file, image_processing
 from PIL import Image
 
 
@@ -164,35 +164,9 @@ def upload_school_image(id):
     if pic and allowed_file(pic.filename):
 
         filename = secure_filename(pic.filename)
+        folder_location = current_app.config['IMAGE_UPLOADS_SCHOOL']
 
-        # Saves the original file with original name and filetype in while creating a new unique id directory
-        newdir = (os.path.join(current_app.config['IMAGE_UPLOADS_SCHOOL'], id))
-        if not os.path.exists(newdir):
-            os.makedirs(newdir)
-        pic.save(os.path.join(newdir, filename))
-
-        # Transforms uploaded image into .png file, name changing into [id].png
-        pic = Image.open(pic)
-        pic.save(os.path.join(newdir, id + '.png'))
-
-        # Saves the image in predetermined "full size" dimensions (590, 330). Saves it as [id]_full.png in proper directory
-        full_size = pic.resize((590, 330))
-        full_size.save(os.path.join(newdir, id + '_full.png'))
-
-        # Image cropping and resising into the "full size" dimensions. Alternative to full size. Saving it as [id]_cropped_full.png
-        cw, ch = 590, 330
-        w, h = pic.size
-        box = w // 2 - cw // 2, h // 2 - ch // 2, w // 2 + cw // 2, h // 2 + ch // 2
-        cropped_image = pic.crop(box)
-        cropped_image.save(os.path.join(newdir, id + '_cropped_full.png'))
-
-        # Creates thumbnail, saving as [id]_thumb.png
-        thumb_size = pic.resize((115, 90))
-        thumb_size.save(os.path.join(newdir, id + '_thumb.png'))
-
-        resp = jsonify({'message': 'Image successfully uploaded'})
-        resp.status_code = 201
-        return resp
+        image_processing(pic, id, filename,folder_location)
 
     else:
         resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
--- Flask/app/utils/images.py
+++ Flask/app/utils/images.py
@@ -1,20 +1,43 @@
 from PIL import Image
 import os
+from flask import jsonify
 from instance.config import ALLOWED_EXTENSIONS
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def image_processing(pic, id, filename, folder_location):
+    # Saves the original file with original name and filetype in while creating a new unique id directory
+    newdir = (os.path.join(folder_location, id))
+    if not os.path.exists(newdir):
+        os.makedirs(newdir)
+    pic.save(os.path.join(newdir, filename))
 
+    # Transforms uploaded image into .png file, name changing into [id].png
+    pic = Image.open(pic)
+    pic.save(os.path.join(newdir, id + '.png'))
 
+    # Saves the image in predetermined "full size" dimensions (590, 330). Saves it as [id]_full.png in proper directory
+    full_size = pic.resize((590, 330))
+    full_size.save(os.path.join(newdir, id + '_full.png'))
 
+    # Image cropping and resising into the "full size" dimensions. Alternative to full size. Saving it as [id]_cropped_full.png
+    cw, ch = 590, 330
+    w, h = pic.size
+    box = w // 2 - cw // 2, h // 2 - ch // 2, w // 2 + cw // 2, h // 2 + ch // 2
+    cropped_image = pic.crop(box)
+    cropped_image.save(os.path.join(newdir, id + '_cropped_full.png'))
 
+    # Creates thumbnail, saving as [id]_thumb.png
+    thumb_size = pic.resize((115, 90))
+    thumb_size.save(os.path.join(newdir, id + '_thumb.png'))
 
+    resp = jsonify({'message': 'Image successfully uploaded'})
+    resp.status_code = 201
+    return resp
 
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-#
-#
 # def change_image_type(image_type):  # <--- Changes the image type into the .png standard format
 #
 #     image = Image.open('demo_image.jpg')
