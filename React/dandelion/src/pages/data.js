90f5caedb2e9159f5798d7cb79db9444440d8ec7
Aaron <aaron.jcam98@gmail.com>
2022-03-25 13:34:09 +0000

    Data page css setup.

--- React/dandelion/src/pages/data.js
+++ React/dandelion/src/pages/data.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 import Header from "../components/navigation/header"
-import '../styles/App.scss'
-import Card from '../components/cards/projectCard';
-import TuneIcon from '@mui/icons-material/Tune';
-import { readRecord } from '../utils/CRUD';
+import "../styles/App.scss"
+import Card from "../components/cards/projectCard"
+import TuneIcon from "@mui/icons-material/Tune"
+import { readRecord } from "../utils/CRUD"
 export default function Data() {
-  const [projectList, setData] = useState(0);
+  const [projectList, setData] = useState(0)
 
   useEffect(() => {
     // Update the document title using the browser API
@@ -16,6 +16,27 @@ export default function Data() {
     <div>
       <Header />
       <div className="data">
+      <div className="data-container">
+        <div className="data-content">
+          <div className="filter-list">
+            <div className="title">
+              <h3>Filter List</h3>
+            </div>
+            <div className="filters">
+              <h2>filters</h2>
+            </div>
+          </div>
+          <div className="spacer" />
+          <div className="data-pane">
+            <div className="tabs"></div>
+
+            <div className="pane-content">
+              <div className="chart-table-view"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="data">
         <div className="container">
           <div className="heading">
             <h1>Data Page</h1>
@@ -29,7 +50,7 @@ export default function Data() {
               : <h3>No activities found</h3>}
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
-  );
+  )
 }
