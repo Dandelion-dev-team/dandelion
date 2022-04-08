f85702445043d21a1fc142a4e7f49ef7335df330
Notis Siokas <76698442+NotisSiokas@users.noreply.github.com>
2022-04-08 22:22:42 +0100

    observation UPDATE + DELETE route with Auditing AND experiment participant Creation, Updating status, Deleting

--- Flask/app/admin/views/experiment_participant.py
+++ Flask/app/admin/views/experiment_participant.py
@@ -2,6 +2,7 @@ from flask import abort, request, jsonify
 from flask_cors import cross_origin
 from flask_json import json_response
 from flask_jwt_extended import get_jwt_identity, jwt_required
+from sqlalchemy import inspect
 from sqlalchemy.sql.functions import user
 import os
 from app.utils.uploads import get_uploaded_file, content_folder
@@ -10,7 +11,7 @@ from app.admin import admin
 from app.models import ExperimentParticipant
 from app.models import Experiment
 from app import db
-from app.utils.auditing import audit_create
+from app.utils.auditing import audit_create, prepare_audit_details, audit_delete, audit_update
 from app.utils.functions import row2dict, jwt_user
 
 
@@ -19,6 +20,33 @@ from app.utils.functions import row2dict, jwt_user
 def listExperiment_participant():
     experiment_participant = ExperimentParticipant.query.all()
     return json_response(data=(row2dict(x, summary=True) for x in experiment_participant))
+
+
+@admin.route('/experiment_participant/add', methods=['POST'])
+@cross_origin(origin='http://127.0.0.1:8000/', supports_credentials='true')
+@jwt_required()
+def addExperiment_participant():
+    current_user = jwt_user(get_jwt_identity())
+    data = request.get_json()
+
+    experiment_participant = ExperimentParticipant(
+        user_id=data['user_id'],
+        experiment_id=data['experiment_id'],
+        status=data['status'],
+    )
+
+    db.session.add(experiment_participant)
+    return_status = 200
+    message = "New experiment participant has been registered"
+
+    try:
+        db.session.commit()
+        audit_create("experiment_participant", experiment_participant.id, current_user.id)
+        return {"message": message, "id": experiment_participant.id}
+
+    except Exception as e:
+        db.session.rollback()
+        abort(409, e.orig.msg)
 
 
 @admin.route('/experiment_participant/<int:user_id>', methods=['GET'])
@@ -36,7 +64,6 @@ def experiments_by_participant(user_id):
         experiment_data['image_thumb'] = os.path.join(content_folder('project', id, 'image'), 'thumb.png')
 
         output.append(experiment_data)
-
 
     return jsonify({'data': output})
 
@@ -65,3 +92,53 @@ def add_new_experiment_participant(experiment_id, user_id):
     except Exception as e:
         db.session.rollback()
         abort(409, e.orig.msg)
+
+
+@admin.route('/experiment_paticipant/updatestatus/<int:experiment_paticipant_id>', methods=['GET', 'PUT'])
+@cross_origin(origin='http://127.0.0.1:8000/', supports_credentials='true')
+@jwt_required()
+def updateExperimentParticipantStatus(experiment_paticipant_id):
+    current_user = jwt_user(get_jwt_identity())
+    experiment_participant_to_update = ExperimentParticipant.query.get_or_404(experiment_paticipant_id)
+    new_data = request.get_json()
+
+    experiment_participant_to_update.status = new_data['status']
+
+    audit_details = prepare_audit_details(inspect(ExperimentParticipant), experiment_participant_to_update,
+                                          delete=False)
+
+    message = "Experiment participant status has been updated"
+
+    if len(audit_details) > 0:
+        try:
+            db.session.commit()
+            audit_update("experiment_participant", experiment_participant_to_update.id, audit_details, current_user.id)
+            return jsonify({"message": message})
+
+        except Exception as e:
+            db.session.rollback()
+            abort(409)
+
+
+@admin.route('/experiment_paticipant/delete/<int:experiment_paticipant_id>', methods=['DELETE'])
+@cross_origin(origin='http://127.0.0.1:8000/', supports_credentials='true')
+@jwt_required()
+def deleteΕxperimentPaticipant(experiment_paticipant_id):
+    current_user = jwt_user(get_jwt_identity())
+    experiment_participant_to_delete = ExperimentParticipant.query.filter_by(id=experiment_paticipant_id).first()
+    if not experiment_participant_to_delete:
+        return jsonify({"message": "No experiment participant found!"})
+
+    audit_details = prepare_audit_details(inspect(ExperimentParticipant), experiment_participant_to_delete, delete=True)
+    db.session.delete(experiment_participant_to_delete)
+    return_status = 200
+    message = "The Experiment Participant has been deleted"
+
+    try:
+        db.session.commit()
+        audit_delete("experiment_participant", experiment_participant_to_delete.id, audit_details, current_user.id)
+        return jsonify({"message": message})
+
+    except Exception as e:
+        db.session.rollback()
+        abort(409)
