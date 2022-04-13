de400d091bdfea34c57f1ef4603218d51d759599
Notis Siokas <76698442+NotisSiokas@users.noreply.github.com>
2022-04-13 23:46:54 +0100

    The GET route returns any node alerts with a status of active
    PUT routes updates the node alert status to cleared (getting "cleared" from the body). It gets only "cleared" and then updates status and updated_date to datetime.now

--- Flask/app/admin/views/node_alert.py
+++ Flask/app/admin/views/node_alert.py
@@ -1,12 +1,15 @@
-from flask import abort, jsonify
+from datetime import datetime
+
+from flask import abort, jsonify, request
 from flask_cors import cross_origin
 from flask_json import json_response
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from sqlalchemy import DATETIME
 
 from app.admin import admin
 from app.models import Node_alert
 from app import db
-from app.utils.functions import row2dict
+from app.utils.functions import row2dict, jwt_user
 
 
 @admin.route('/node_alert', methods=['GET'])
@@ -34,3 +37,26 @@ def listActiveNode_alerts():
             output.append(node_alerts_data)
 
     return jsonify({'node_alerts with status ACTIVE': output})
+
+
+@admin.route('/node_alert/<int:node_alert_id>', methods=['PUT'])
+@cross_origin(origin='http://127.0.0.1:8000/', supports_credentials='true')
+@jwt_required()
+def update_node_alert_status(node_alert_id):
+    current_user = jwt_user(get_jwt_identity())
+    node_alert_to_update = Node_alert.query.get_or_404(node_alert_id)
+    new_node_alert_data = request.get_json()
+
+    node_alert_to_update.status = new_node_alert_data['status']
+    new_status = node_alert_to_update.status
+
+    if new_status == "cleared":
+        message = "Node alert status has been set to 'cleared'"
+        node_alert_to_update.updated_date = datetime.now()
+        db.session.commit()
+        return {"message": message}
+    else:
+        message = "Node alert status has not been updated, please insert correct Status Value"
+
+    return jsonify({message})
+
